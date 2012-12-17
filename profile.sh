c5ee832c1792feba00a909db22d670bd78a3d62e
Doug <doug@neverfear.org>
2012-12-17 21:33:03 +0000

    Updated shell script for 1.0.0

--- profile.sh
+++ profile.sh
@@ -4,8 +4,8 @@ eventCount=100000000
 for benchmarkType in LATENCY THROUGHPUT; do
 	for taskLength in `seq 0 500 1000000`; do 
   	  for processorType in MERGE LINKED TICKET; do
-        	echo java -jar mergingbatcheventprocessor-0.0.1-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
-        	java -jar mergingbatcheventprocessor-0.0.1-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
+        	echo java -jar mergingbatcheventprocessor-1.0.0-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
+        	java -jar mergingbatcheventprocessor-1.0.0-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
     	done
     done
 done
