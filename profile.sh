15b4a24f3d58b8ae2e3dea32e7cd43107570b173
Doug <doug@neverfear.org>
2012-12-17 23:33:30 +0000

    Profile now only considers an task length in various magnitudes rather every half microsecond. This will reduce the noise in the output statistics as the task length increases.

--- profile.sh
+++ profile.sh
@@ -2,7 +2,7 @@
 
 eventCount=100000000
 for benchmarkType in LATENCY THROUGHPUT; do
-	for taskLength in `seq 0 500 1000000`; do 
+	for taskLength in 0 1000 10000 100000 1000000 10000000; do 
   	  for processorType in MERGE LINKED TICKET; do
         	echo java -jar mergingbatcheventprocessor-1.0.0-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
         	java -jar mergingbatcheventprocessor-1.0.0-SNAPSHOT-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
