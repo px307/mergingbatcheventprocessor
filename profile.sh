2d5c68c2f7206ef5825b4b5c035cf7bba01c1983
Doug <doug@neverfear.org>
2012-12-18 00:49:53 +0000

    Profile script uses the "target" directory for the jar location

--- profile.sh
+++ profile.sh
@@ -4,8 +4,8 @@ eventCount=100000000
 for benchmarkType in LATENCY THROUGHPUT; do
 	for taskLength in 0 1000 10000 100000 1000000 10000000; do 
   	  for processorType in MERGE LINKED TICKET; do
-        	echo java -jar mergingbatcheventprocessor-1.0.0-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
-        	java -jar mergingbatcheventprocessor-1.0.0-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
+        	echo java -jar target/mergingbatcheventprocessor-1.0.0-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
+        	java -jar target/mergingbatcheventprocessor-1.0.0-benchmark.jar $benchmarkType $processorType $taskLength $eventCount
     	done
     done
 done
