fa941db0e8441fdb7011cb13b4a59b3a6ec0ca53
Doug <doug@neverfear.org>
2013-02-27 20:59:17 +0000

    Improved jdoc

--- src/main/java/org/neverfear/disruptor/MergeStrategy.java
+++ src/main/java/org/neverfear/disruptor/MergeStrategy.java
@@ -4,7 +4,8 @@ public interface MergeStrategy<E> {
 
 	/**
 	 * Give the merging batch processor a hint about the size of your key space. This is used to back the merging
-	 * collections and avoid resizing during processing.
+	 * collections and avoid resizing during processing. Where your key space is dynamic try to pick a sensible worse
+	 * case scenario.
 	 * 
 	 * @return
 	 */
